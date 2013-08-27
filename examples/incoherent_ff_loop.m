26046a357b618075686b7212f0e34a34dff58f85
Vipul Singhal <11724197+vipulsinghal02@users.noreply.github.com>
2013-08-27 09:27:02 +0000

    Added IFFL for Tue Workshop HW. Removed old versions of FFL and Lucks circuits to avoid confusion.

--- examples/incoherent_ff_loop.m
+++ examples/incoherent_ff_loop.m
@@ -1,115 +1,56 @@
+% IFFL as tested in lab by S. Guo
+% VS 2013
+
 %% clean up
 
-% clear variables
-% clc
-% close all
-%% My test run
+clear all
+close all
+clc
 
+
+%% no clpX
 % Set up the standard TXTL tubes
-tube1 = txtl_extract('E7');
-tube2 = txtl_buffer('E7');
-
-% Set up a tube that will contain our DNA
-tube3 = txtl_newtube('circuit_open_loop');
-txtl_add_dna(tube3, ...
-    'p70(50)', 'rbs(20)', 'sigma28(600)', 0.2, 'plasmid');
-txtl_add_dna(tube3, ...
-    'p28_ptet(150)', 'rbs(20)', 'deGFP(1000)', 1, 'plasmid');
 
 
+tube1 = txtl_extract('E9');
+tube2 = txtl_buffer('E9');
 
-tube4 = txtl_newtube('circuit_closed_loop');
-txtl_add_dna(tube4, ...
-    'p28(50)', 'rbs(20)', 'tetR(600)', 0.01, 'plasmid');
-txtl_add_dna(tube4, ...
-    'p70(50)', 'rbs(20)', 'sigma28(600)',0.2, 'plasmid');
-txtl_add_dna(tube4,'p28_ptet(150)', 'rbs(20)', 'deGFP(1000)',1, 'plasmid');
+tube3 = txtl_newtube('circuit_closed_loop_withClpX');
 
+txtl_add_dna(tube3, 'p70(50)', 'rbs(20)', 'AraC(600)',0.04*4.5, 'plasmid');
+txtl_add_dna(tube3, 'pBAD(50)', 'rbs(20)', 'tetR(600)', 0.05*4.5, 'plasmid');
+txtl_add_dna(tube3,'pBAD_ptet(150)', 'rbs(20)', 'deGFP(1000)-lva(20)',0.5*4.5, 'plasmid');
+txtl_add_dna(tube3,'p70(50)', 'rbs(20)', 'ClpX(600)',0.1*4.5, 'plasmid');
 
-
-
-% Mix the contents of the individual tubes
-well_a1 = txtl_combine([tube1, tube2, tube3]);
-
-% set up well_b1
-well_b1 = txtl_combine([tube1, tube2, tube4]);
-
-
-
-
-
-
+txtl_addspecies(tube3, 'arabinose', 1000);
+txtl_addspecies(tube3, 'aTc', 1000);
+txtl_addspecies(tube3, 'protein ClpX', 100);
+% set up well_a1
+Mobj = txtl_combine([tube1, tube2, tube3]);
 
 %% Run a simulation
+ simulationTime = 12*60*60;
 
-simulationTime = 12*60*60;
+simData = txtl_runsim(Mobj,simulationTime);
 
+t_ode = simData.Time;
+x_ode = simData.Data;
 
-% 1st run
-[t_ode,x_ode] = txtl_runsim(well_a1,simulationTime);
-
-[t_ode_b1,x_ode_b1] = txtl_runsim(well_b1,simulationTime);
-
-
-%% plot the result
-close all
-% DNA and mRNA plot
-dataGroups{1,1} = 'DNA and mRNA';
-dataGroups{1,2} = {'ALL_DNA'};
-dataGroups{1,3} = {'b-','r-','g--','r--','y-','c-','g-','g--'};
-
-% Gene Expression Plot
-dataGroups{2,1} = 'Gene Expression';
-dataGroups{2,2} = {'protein deGFP*','protein tetRdimer'};
-%dataGroups{2,3} = {'b-','g--','g-','r-','b--','b-.'};
-
-% Resource Plot
-dataGroups{3,1} = 'Resource usage';
-%
-txtl_plot(t_ode,x_ode,well_a1,dataGroups);
-
-txtl_plot(t_ode_b1,x_ode_b1,well_b1,dataGroups);
-
-% figure(3)
-% hold on
-% plot(t_ode_b1/60,x_ode_b1(:,findspecies(well_b1,'protein tetR')),'b')
-% plot(t_ode_b1/60,x_ode_b1(:,findspecies(well_b1,'protein deGFP*')),'g')
-% plot(t_ode_b1/60,x_ode_b1(:,findspecies(well_b1,'protein tetRdimer')),'r')
-% xlabel('Time [min]');
-% ylabel('Concentration [nM]');
-% hold off
-% legend('tetR','deGFP*','tetRdimer')
-
-figure(4)
-hold on
-plot(t_ode_b1/60,x_ode_b1(:,findspecies(well_b1,'RNAP28:DNA p28_ptet--rbs--deGFP')),'b')
-plot(t_ode_b1/60,x_ode_b1(:,findspecies(well_b1,'DNA p28_ptet--rbs--deGFP:protein tetRdimer')),'r')
-plot(t_ode_b1/60,x_ode_b1(:,findspecies(well_b1,'RNAP28:DNA p28_ptet--rbs--deGFP:protein tetRdimer')),'g')
-xlabel('Time [min]');
-ylabel('Concentration [nM]');
-hold off
-legend('DNA:RNAP28','DNA:tetR','DNA:RNAP28:tetR','Location','Best')
-
-
-figure(5)
-hold on
-plot(t_ode_b1/60,x_ode_b1(:,findspecies(well_b1,'RNAP')),'b')
-plot(t_ode_b1/60,x_ode_b1(:,findspecies(well_b1,'RNAP28')),'r')
-plot(t_ode_b1/60,x_ode_b1(:,findspecies(well_b1,'RNAP70')),'g')
-xlabel('Time [min]');
-ylabel('Concentration [nM]');
-hold off
-legend('RNAP','RNAP28','RNAP70')
-
-
-figure(6)
-hold on
-plot(t_ode/60,x_ode(:,findspecies(well_a1,'protein deGFP*')),'b')
-plot(t_ode_b1/60,x_ode_b1(:,findspecies(well_b1,'protein deGFP*')),'r')
-
-xlabel('Time [min]');
-ylabel('Concentration [nM]');
-hold off
-legend('open loop - deGFP*','closed loop - deGFP*','Location','Best')
-
+figure
+cellOfSpecies = {'RNAP70:DNA pBAD_ptet--rbs--deGFP-lva:protein tetRdimer', 'RNAP70:DNA pBAD_ptet--rbs--deGFP-lva:protein tetRdimer:arabinose:protein AraC'
+                 'NTP:RNAP70:DNA pBAD_ptet--rbs--deGFP-lva:protein tetRdimer','NTP:RNAP70:DNA pBAD_ptet--rbs--deGFP-lva:protein tetRdimer:arabinose:protein AraC'
+                 'RNAP70:DNA pBAD_ptet--rbs--deGFP-lva:arabinose:protein AraC', 'RNAP70:DNA pBAD--rbs--tetR:arabinose:protein AraC'
+                 'NTP:RNAP70:DNA pBAD_ptet--rbs--deGFP-lva:arabinose:protein AraC', 'NTP:RNAP70:DNA pBAD--rbs--tetR:arabinose:protein AraC'};
+plotCustomSpecies2({Mobj}, {x_ode}, {t_ode}, cellOfSpecies)
+figure
+cellOfSpecies = {'protein deGFP-lva*', 'protein tetR','protein ClpX*'
+                 'protein AraC','protein tetRdimer','protein deGFP-lva*:protein ClpX*'
+                 'arabinose', 'aTc','protein deGFP-lva***'
+                 'arabinose:protein AraC', '2 aTc:protein tetRdimer','protein ClpX'};
+plotCustomSpecies2({Mobj}, {x_ode}, {t_ode}, cellOfSpecies)
+figure
+cellOfSpecies = {'RNAP', 'protein sigma70','Ribo'
+                 'RNAP70','RNase','NTP'
+                 'ATP', 'AA','protein deGFP-lva'};
+plotCustomSpecies2({Mobj}, {x_ode}, {t_ode}, cellOfSpecies)
 
